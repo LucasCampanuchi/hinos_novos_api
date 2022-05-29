9955c9b8a335ea9830ccbb12d5fb5e9d93db599a
Lucas Corrêa <lucasccorrea09@hotmail.com>
2022-05-29 13:47:10 -0300

    Alterações nas configuraçoes de conexão

--- database/connectionBd.js
+++ database/connectionBd.js
@@ -1,17 +1,26 @@
 const { Sequelize } = require('sequelize');
+const pg = require('pg');
+pg.defaults.ssl = true;
 
 /* const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: 'database/db.sqlite'
 }); */
 
-const sequelize = new Sequelize(
-    `postgres://${(process.env.DB_USER ?? 'postgres')}@${(process.env.DB_HOST ?? 'localhost')}:${(process.env.DB_PORT ?? '5432')}/${(process.env.DB_NAME ?? 'hinos')}`, 
-    {
-        dialect: 'postgres',
-        password: process.env.DB_PASSWORD ?? '1234'
-    }
-);
+const sequelize = new Sequelize({
+    database: process.env.DB_NAME ?? 'hinos',
+    username: process.env.DB_USER ?? 'postgres',
+    password: process.env.DB_PASSWORD ?? '1234',
+    host: process.env.DB_HOST ?? 'localhost',
+    port: process.env.DB_PORT ?? 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+      }
+    },
+  });
 
 module.exports = sequelize
 
